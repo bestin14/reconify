b380d1e67c37e3094b25f04672623a98752f3c77
bestin14 <bestinbabu6@gmail.com>
2022-07-20 21:31:20 +0530

    color

--- reconify.py
+++ reconify.py
@@ -247,7 +247,7 @@ def choose() :
     final_urls()
 
     cprint('Urls from the domain has been collected and saved to final_urls file \n Removing unwanted files \n','cyan')
-    cprint('Make sure to collect urls from burp spidering also!!')
+    cprint('Make sure to collect urls from burp spidering also!!','cyan')
     cmd = ['sh', '-c', 'rm xlinkfinder_urls jsfinder_urls sub_js js_urls xlinks gospider_urls gau_urls wayback_urls hi from_links hakrawler_urls']
     subprocess.run(cmd,check=True,stderr=subprocess.DEVNULL,stdout=subprocess.DEVNULL)    
 
